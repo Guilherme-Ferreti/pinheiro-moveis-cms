d41541f52b21da43dbc74d852f12b47e727448a8
Guilherme-Ferreti <guiferretis@gmail.com>
2021-06-29 09:43:04 -0300

    UsersSeeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(StatesSeeder::class);
         $this->call(ProductsSeeder::class);
+        $this->call(UsersSeeder::class);
     }
 }
