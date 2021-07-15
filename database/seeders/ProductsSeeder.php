361e14054adf122c76ab51cb7507b682780cd4e1
Guilherme-Ferreti <guiferretis@gmail.com>
2021-07-15 08:50:42 -0300

    ProductSeeder refactored

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -21,23 +21,18 @@ class ProductsSeeder extends Seeder
         $products = Product::factory(10)->create();
         $states = State::all();
 
-        foreach ($products as $product) {
-            File::factory(5)->create([
-                'fileable_id' => $product->id,
-                'fileable_type' => Product::class,
-            ]);
+        $products->each(function ($product) use ($states, $priceList) {
+            $product->files()->createMany(File::factory(5)->make()->toArray());
 
-            $prices = [];
-
-            foreach ($states as $state) {
-                $prices[] = [
+            $prices = $states->map(function ($state) use ($priceList) {
+                return [
                     'price_list_id' => $priceList->id,
                     'state_id' => $state->id,
                     'amount' => rand(1, 5000) / 10
                 ];
-            }
-            
+            });
+
             $product->prices()->createMany($prices);
-        }
+        });
     }
 }
